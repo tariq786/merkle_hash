c29f17f3880f8f39f773ecc14ed13f55c6e95674
tariq786 <tariqbashir@gmail.com>
2016-03-26 14:43:14 -0400

    correcting insert function

--- tree.py
+++ tree.py
@@ -1,9 +1,11 @@
 from collections import deque
 
-branch_bits = ''
-parents = deque([])
-path  = deque([])
-val = ''
+#branch_bits = ''
+#parents = deque([])
+#path  = deque([])
+#val = ''
+#prev_bits=''
+#prev_root=None
 
 class Node(object):
     def __init__(self,bits,data):
@@ -22,6 +24,12 @@ class Tree(object):
     def __init__(self):
         self.root = None
 
+        self.branch_bits = ''
+        self.parents = deque([])
+        self.path = deque([])
+        self.prev_bits = ''
+        self.prev_root = None
+
     def longest_common_substring(self,s1, s2):
         m = [[0] * (1 + len(s2)) for i in xrange(1 + len(s1))]
         longest, x_longest = 0, 0
@@ -39,88 +47,103 @@ class Tree(object):
     def has_Key(self,key):
         return ''
 
-    def insert_Item(self,root,key,datastr):
+    def insert_Item(self,key,datastr):
         # Single node case, where only one key has already been inserted,root points to leaf node
-        if (root.data != '' and len(root.bits) == 4):
-            prev_root = root      #save prev root
-            prev_bits = root.bits #save prev node's bits or key
+        if (len(self.root.bits) == 4):
+            self.prev_root = self.root      #save prev root
+            self.prev_bits = self.root.bits #save prev node's bits or key
             i = Inner()           # create new inner node
-            common = self.longest_common_substring(prev_bits,key) #function call to determine common bits
+            common = self.longest_common_substring(self.prev_bits,key) #function call to determine common bits
             i.bits = common
-            root = i                                  # set new root as inner node
+            self.root = i                                  # set new root as inner node
             n = Node(key,datastr)                     # create new leaf node
-            if(prev_bits[len(common)] == '0'):
-                root.c0 = prev_root
-                root.c1 = n
+            if(self.prev_bits[len(common)] == '0'):
+                self.root.c0 = self.prev_root
+                self.root.c1 = n
             else:
-                root.c1 = prev_root
-                root.c0 = n
-            prev_bits = prev_bits[len(common)+1:]  # remove common bits + branch bit from old node
+                self.root.c1 = self.prev_root
+                self.root.c0 = n
+            self.prev_bits = self.prev_bits[len(common)+1:]  # remove common bits + branch bit from old node
             n.key = key[len(common)+1:]            # remove common bits + branch bit from new node
         # inner node with empty bits
-        elif(root.bits == '' and (root.c0 != None or root.c1 != None) ):
+        elif(self.root.bits == '' and (self.root.c0 != None or self.root.c1 != None) ):
             #First find the appropriate place to insert the new key
             msb = key[0]
-            branch_bits += msb
-            parents.append(root)
-            path.append(msb)
+            self.branch_bits += msb
+            self.parents.append(self.root)
+            self.path.append(msb)
             if (msb):
-                self.insert_Item(root.c1, key[1:], datastr) #eventually takes you to leaf
+                self.insert_Item(self.root.c1, key[1:], datastr) #eventually takes you to leaf
             else:
-                self.insert_Item(root.c0, key[1:], datastr) #eventually takes you to leaf
+                self.insert_Item(self.root.c0, key[1:], datastr) #eventually takes you to leaf
 
         # inner node with non-empty bits
-        elif (root.bits != '' and (root.c0 != None or root.c1 != None)):  # inner node with bits
-            prev_bits = branch_bits + root.bits
-            common = self.longest_common_substring(prev_bits, key[0:len(prev_bits)])
-            if common == '': #inser new inner node above this inner node (Github C=1111 insertion)
+        elif (self.root.bits != '' and (self.root.c0 != None or self.root.c1 != None)):  # inner node with bits
+            self.prev_bits = self.branch_bits + self.root.bits
+            common = self.longest_common_substring(self.prev_bits, key[0:len(self.prev_bits)])
+
+            if (common == '' or  ( len(common) < len(self.root.bits) ) ): #inser new inner node above this inner node (Github C=1111 insertion)
                 i = Inner()  # create new inner node
                 n = Node(key, datastr)  # create new leaf node
-                path_prev_bit = path.pop()
-                if path_prev_bit == '0':
-                    i.c0 = root
-                    i.c1 = n
+                if len(self.path) != 0:
+                    path_prev_bit = self.path.pop()
+                    if path_prev_bit == '0':
+                        i.c0 = self.root
+                        i.c1 = n
+                    else:
+                        i.c1 = self.root
+                        i.c0 = n
+
+                if self.root.bits[0] == 0:
+                    i.c0 = self.root
                 else:
-                    i.c1 = root
-                    i.c0 = n
+                    i.c1 = self.root
+                i.bits = common ##double check this value
+                self.root.bits = self.root.bits[len(common)+1:]
+
                 #prev_bits = prev_bits[len(common) + 1:]  # remove common bits + branch bit from old node
-                root.bits = root.bit[1:] #decrease bit due to branching
+                #self.root.bits = self.root.bits[1:] #decrease bit due to branching
                 n.key = key[len(common) + 1:]  # remove common bits + branch bit from new node
+
                 # update the parent of i
-                if len(parents) != 0:
-                    parent = parents.pop()
+                if len(self.parents) != 0:
+                    parent = self.parents.pop()
                     if(path_prev_bit == 0):
                         parent.c0 = i  # check whether c0 or c1
                     else:
                         parent.c1 = i  # check whether c0 or c1
                 else:
-                    root = i
-                i.bits = ''  # common[1:]  # remove MSB bits from the inner node as it got moved to the branch
-                branch_bits = ''
+                    self.root = i
 
-        elif (root.data != '' and len(root.bits) < 4):  # leaf node reached
+                self.branch_bits = ''
+                self.prev_bits = ''
+
+
+        elif ( isinstance(self.root,Node) and len(self.root.bits) < 4  ):  # leaf node reached
             # create inner node in place of leaf node
-            prev_bits = branch_bits + root.bits  # this retrieves leaf node's key
+            self.prev_bits = self.branch_bits + self.root.bits  # this retrieves leaf node's key
             i = Inner()  # create new inner node
-            common = self.longest_common_substring(prev_bits, key)
+            common = self.longest_common_substring(self.prev_bits, key)
             #i.bits = common
             n = Node(key, datastr)  # create new leaf node
-            if (prev_bits[len(common) + 1] == '0'):
-                i.c0 = root
+            if (self.prev_bits[len(common) + 1] == '0'):
+                i.c0 = self.root
                 i.c1 = n
             else:
-                i.c1 = root
+                i.c1 = self.root
                 i.c0 = n
-            prev_bits = prev_bits[len(common) + 1:]  # remove common bits + branch bit from old node
+            self.prev_bits = self.prev_bits[len(common) + 1:]  # remove common bits + branch bit from old node
             n.key = key[len(common) + 1:]  # remove common bits + branch bit from new node
             # update the parent of i
-            parent = parents.pop()
+            parent = self.parents.pop()
             parent.c0 = i  # check whether c0 or c1
             i.bits = common[1:]  # remove MSB bits from the inner node as it got moved to the branch
             # check if you need to borrow bit
             parent.bits = i.bits[0]
             i.bits = i.bits[1:]  # check if an expression is needed instead of ''
             branch_bits = ''
+        else:
+            print "not found"
 
 
 
@@ -131,7 +154,7 @@ class Tree(object):
             self.root = n
         else:
             #if self.has_Key(key) == '': #Key does not already exist
-            self.insert_Item(self.root,key,datastr)
+            self.insert_Item(key,datastr)
             #else:
             #    print "Key already present"
 
@@ -142,5 +165,6 @@ if __name__ == '__main__':
     t = Tree()
     t.insert('0000','A')
     t.insert('0001','B')
+    t.insert('0010','C')
 
 
